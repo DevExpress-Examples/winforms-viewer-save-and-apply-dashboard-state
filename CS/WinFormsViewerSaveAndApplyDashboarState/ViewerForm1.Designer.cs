65b9be97e069a69ac8b4d62c7c6d3e77ca93f100
Natalia Kazakova <natalia.kazakova@devexpress.com>
2019-06-11 11:59:14 +0300

    add cs solution

---
+++ CS/WinFormsViewerSaveAndApplyDashboarState/ViewerForm1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WinFormsViewerSaveAndApplyDashboarState
+{
+    partial class ViewerForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.dashboardViewer = new DevExpress.DashboardWin.DashboardViewer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dashboardViewer
+            // 
+            this.dashboardViewer.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(236)))), ((int)(((byte)(239)))));
+            this.dashboardViewer.Appearance.Options.UseBackColor = true;
+            this.dashboardViewer.DashboardSource = "";
+            this.dashboardViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dashboardViewer.Location = new System.Drawing.Point(0, 0);
+            this.dashboardViewer.Name = "dashboardViewer";
+            this.dashboardViewer.Size = new System.Drawing.Size(986, 628);
+            this.dashboardViewer.TabIndex = 0;
+            this.dashboardViewer.UseNeutralFilterMode = true;
+            this.dashboardViewer.DashboardLoaded += new DevExpress.DashboardWin.DashboardLoadedEventHandler(this.dashboardViewer_DashboardLoaded);
+            this.dashboardViewer.SetInitialDashboardState += new DevExpress.DashboardWin.SetInitialDashboardStateEventHandler(this.dashboardViewer_SetInitialDashboardState);
+            // 
+            // ViewerForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 628);
+            this.Controls.Add(this.dashboardViewer);
+            this.Name = "ViewerForm1";
+            this.Text = "Dashboard Viewer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ViewerForm1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.DashboardWin.DashboardViewer dashboardViewer;
+    }
+}
+
---
+++ CS/WinFormsViewerSaveAndApplyDashboarState/ViewerForm1.cs
@@ -0,0 +1,43 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.XtraEditors;
+using System;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WinFormsViewerSaveAndApplyDashboarState
+{
+    public partial class ViewerForm1: XtraForm
+    {
+        DashboardState dState = new DashboardState();
+        const string path = @"..\..\Dashboards\dashboardWithSavedState.xml";
+        public ViewerForm1() {
+            InitializeComponent();
+            dashboardViewer.DashboardSource = path;
+        }
+
+        private void dashboardViewer_DashboardLoaded(object sender,
+            DevExpress.DashboardWin.DashboardLoadedEventArgs e) {
+            XElement data = e.Dashboard.UserData;
+                if(data != null) {
+                    if(data.Element("DashboardState") != null) {
+                        XDocument dStateDocument = XDocument.Parse(data.Element("DashboardState").Value);
+                        dState.LoadFromXml(XDocument.Parse(data.Element("DashboardState").Value));
+                    }
+                }
+            }
+
+        private void dashboardViewer_SetInitialDashboardState(object sender,
+            DevExpress.DashboardWin.SetInitialDashboardStateEventArgs e) {
+            e.InitialState = dState;
+        }
+
+        private void ViewerForm1_FormClosing(object sender,FormClosingEventArgs e) {
+            dState = dashboardViewer.GetDashboardState();
+            XElement userData = new XElement("Root",
+                new XElement("DateModified",DateTime.Now),
+                new XElement("DashboardState",dState.SaveToXml().ToString(SaveOptions.DisableFormatting)));
+            dashboardViewer.Dashboard.UserData = userData;
+            dashboardViewer.Dashboard.SaveToXml(path);
+        }
+    }
+}
